eaee295696d882e5acc811cbe4e2f2368cf11165
sapnadasarath@gmail.com <sapnadasarath@gmail.com@5cafef7b-8a4f-0cfd-01d2-95dafbb91981>
2011-04-02 22:21:02 +0000

    Adding file UserOptionScreen.java and useraction.xml. this will now contain the main screen code LocationBasedAlerts.java will be a holder to add authentication later

--- trunk/src/com/sunysb/edu/LocationBasedAlerts.java
+++ trunk/src/com/sunysb/edu/LocationBasedAlerts.java
@@ -1,7 +1,6 @@
 package com.sunysb.edu;
 
-import com.sunysb.edu.ui.dialog.Task;
-import com.sunysb.edu.ui.map.Map;
+import com.sunysb.edu.ui.dialog.UserOptionScreen;
 
 import android.app.Activity;
 import android.content.Intent;
@@ -9,54 +8,22 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
-//source : http://mobiforge.com/developing/story/using-google-maps-android
-
 
 public class LocationBasedAlerts extends Activity{
 	
-	private Button addTaskButton;
-	private Button editTaskButton;
-	private Button addFriendsButton;
-	private Button editProfileButton;
-	
-	/** Called when the activity is first created. */
-	@Override
+	private Button okButton;
+
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.main);
 		
-		addTaskButton = (Button) findViewById(R.id.add_Task_button);
-		editTaskButton = (Button) findViewById(R.id.edit_Task_button);
-		addFriendsButton = (Button) findViewById(R.id.organize_Friends_button);
-		editProfileButton = (Button) findViewById(R.id.edit_Profile_button);
+		okButton = (Button) findViewById(R.id.ok_main_button);
 		
-        addTaskButton.setOnClickListener(new View.OnClickListener() {
+		okButton.setOnClickListener(new View.OnClickListener() {
 			@Override
 			public void onClick(View v) {
-				startActivity( new Intent(LocationBasedAlerts.this, Map.class));
+				startActivity(new Intent(LocationBasedAlerts.this, UserOptionScreen.class));
 			}
 		});
-		
-        editTaskButton.setOnClickListener(new View.OnClickListener() {
-			@Override
-			public void onClick(View v) {
-				startActivity(new Intent(LocationBasedAlerts.this, Task.class));
-			}
-		});
-		
-        addFriendsButton.setOnClickListener(new View.OnClickListener() {
-			@Override
-			public void onClick(View v) {
-				startActivity(new Intent(LocationBasedAlerts.this, Map.class));
-			}
-		});
-        
-        editProfileButton.setOnClickListener(new View.OnClickListener() {
-			@Override
-			public void onClick(View v) {
-				startActivity(new Intent(LocationBasedAlerts.this, Map.class));
-			}
-		});
-	}
-	
+	}	
 }
