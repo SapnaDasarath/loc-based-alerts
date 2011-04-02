1dcf5d14e40a68c6ef6cfd2e2f37eb13a482a30b
sapnadasarath@gmail.com <sapnadasarath@gmail.com@5cafef7b-8a4f-0cfd-01d2-95dafbb91981>
2011-04-02 22:21:02 +0000

    Adding file UserOptionScreen.java and useraction.xml. this will now contain the main screen code LocationBasedAlerts.java will be a holder to add authentication later

---
+++ src/com/sunysb/edu/ui/dialog/UserOptionScreen.java
@@ -0,0 +1,60 @@
+package com.sunysb.edu.ui.dialog;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.sunysb.edu.LocationBasedAlerts;
+import com.sunysb.edu.R;
+import com.sunysb.edu.ui.map.Map;
+
+public class UserOptionScreen extends Activity{
+	
+	private Button addTaskButton;
+	private Button editTaskButton;
+	private Button addFriendsButton;
+	private Button editProfileButton;
+	
+	/** Called when the activity is first created. */
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.useraction);
+		
+		addTaskButton = (Button) findViewById(R.id.add_Task_button);
+		editTaskButton = (Button) findViewById(R.id.edit_Task_button);
+		addFriendsButton = (Button) findViewById(R.id.organize_Friends_button);
+		editProfileButton = (Button) findViewById(R.id.edit_Profile_button);
+		
+        addTaskButton.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				startActivity( new Intent(UserOptionScreen.this, Map.class));
+			}
+		});
+		
+        editTaskButton.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				startActivity(new Intent(UserOptionScreen.this, Task.class));
+			}
+		});
+		
+        addFriendsButton.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				startActivity(new Intent(UserOptionScreen.this, Map.class));
+			}
+		});
+        
+        editProfileButton.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				startActivity(new Intent(UserOptionScreen.this, Map.class));
+			}
+		});
+	}
+	
+}
