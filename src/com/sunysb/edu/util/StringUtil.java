e197a13e0cf81fe87895b37f7d2bba6e2d122f7e
sapnadasarath@gmail.com <sapnadasarath@gmail.com@5cafef7b-8a4f-0cfd-01d2-95dafbb91981>
2011-04-06 01:14:43 +0000

    Adding string util file

---
+++ src/com/sunysb/edu/util/StringUtil.java
@@ -0,0 +1,15 @@
+package com.sunysb.edu.util;
+
+public class StringUtil {
+
+	public static final String USER_INFO= "userinfo";
+	public static final String FRIEND_INFO= "friendinfo";
+	
+	public static final String TASK_NAME= "taskname";
+	public static final String TASK_DESCRIPTION= "taskdesc";
+	public static final String TASK_PRIORITY= "taskprior";
+	
+	public static final String PRIOR_LOW= "low";
+	public static final String PRIOR_MED= "med";
+	public static final String PRIOR_HIGH= "high";
+}
