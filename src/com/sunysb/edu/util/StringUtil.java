43724d13d225f79094a57c2a3ea332670e27e4b2
sapnadasarath@gmail.com <sapnadasarath@gmail.com@5cafef7b-8a4f-0cfd-01d2-95dafbb91981>
2011-04-07 07:33:49 +0000

    Adding dynamic table support

--- src/com/sunysb/edu/util/StringUtil.java
+++ src/com/sunysb/edu/util/StringUtil.java
@@ -2,8 +2,12 @@ package com.sunysb.edu.util;
 
 public class StringUtil {
 
+	public static final String USER_ID = "userid";
 	public static final String USER_INFO= "userinfo";
 	public static final String FRIEND_INFO= "friendinfo";
+	
+	public static final String USRNAME = "name";
+	public static final String PASSWD = "password";
 	
 	public static final String TASK_NAME= "taskname";
 	public static final String TASK_DESCRIPTION= "taskdesc";
