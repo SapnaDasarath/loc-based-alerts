d73ece3751d37f5be14d1994c5c3231163741dca
sapnadasarath@gmail.com <sapnadasarath@gmail.com@5cafef7b-8a4f-0cfd-01d2-95dafbb91981>
2011-04-23 20:24:19 +0000

    Adding code to remove task and friend from list

--- trunk/src/com/sunysb/edu/ui/dialog/EditTask.java
+++ trunk/src/com/sunysb/edu/ui/dialog/EditTask.java
@@ -22,65 +22,73 @@ import com.sunysb.edu.util.StringUtil;
 
 public class EditTask extends Activity implements OnTouchListener {
 
+	SimpleDbUtil util;
+
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		this.setContentView(R.layout.taskedit);
-		
-		TableLayout table = (TableLayout) findViewById(R.id.edittask);
-		List<String> taskids = getTasksForUser();
+
 		try {
-			SimpleDbUtil util = new SimpleDbUtil();
-			for(String id : taskids)
-			{
-				HashMap<String, String> taskattributes = util.getAttributesForItem(SimpleDbUtil.getCurrentUser(), id);
-				TableRow tr = new TableRow(this);
-				tr.setLayoutParams(new LayoutParams(LayoutParams.FILL_PARENT,
-						LayoutParams.WRAP_CONTENT));
-				
-				TextView name = new TextView(this);
-				name.setText(taskattributes.get(StringUtil.TASK_NAME)+ "--");
-				name.setTextColor(Color.YELLOW);
-				tr.addView(name);
-				
-				TextView prior = new TextView(this);
-				prior.setText(taskattributes.get(StringUtil.TASK_PRIORITY));
-				prior.setTextColor(Color.YELLOW);
-				tr.addView(prior);
-				
-				table.addView(tr, new TableLayout.LayoutParams(
-						LayoutParams.FILL_PARENT, LayoutParams.WRAP_CONTENT));
-			}
+			util = new SimpleDbUtil();
 		} catch (Exception e) {
 			Log.e("LBA", "Unable to connect to server");
+		}
+		drawUI();
+	}
+
+	private void drawUI() {
+		TableLayout table = (TableLayout) findViewById(R.id.edittask);
+		table.removeAllViews();
+		List<String> taskids = getTasksForUser();
+		for (String id : taskids) {
+			HashMap<String, String> taskattributes = util.getAttributesForItem(
+					SimpleDbUtil.getCurrentUser(), id);
+			TableRow tr = new TableRow(this);
+			tr.setLayoutParams(new LayoutParams(LayoutParams.FILL_PARENT,
+					LayoutParams.WRAP_CONTENT));
+
+			TextView name = new TextView(this);
+			name.setText(taskattributes.get(StringUtil.TASK_NAME) + "--");
+			name.setTextColor(Color.YELLOW);
+			tr.addView(name);
+
+			TextView prior = new TextView(this);
+			prior.setText(taskattributes.get(StringUtil.TASK_PRIORITY));
+			prior.setTextColor(Color.YELLOW);
+			tr.addView(prior);
+
+			table.addView(tr, new TableLayout.LayoutParams(
+					LayoutParams.FILL_PARENT, LayoutParams.WRAP_CONTENT));
 		}
 	}
 
 	private List<String> getTasksForUser() {
 		List<String> domain = new ArrayList<String>();
-		try {
-			SimpleDbUtil util = new SimpleDbUtil();
-			util.getTasksForUser(SimpleDbUtil.getCurrentUser());
-
-		} catch (Exception e) {
-			Log.e("LBA", "Unable to connect to server");
-		}
+		util.getTasksForUser(SimpleDbUtil.getCurrentUser());
 		return domain;
 	}
 
-	//TODO When user selects a task open the edit view for that task
-	//send the task id to open the edit view to get only that task from the db and show it on UI
+	// TODO When user selects a task open the edit view for that task
+	// send the task id to open the edit view to get only that task from the db
+	// and show it on UI
 	@Override
 	public boolean onTouch(View v, MotionEvent event) {
 		// startActivity(new Intent(EditTask.this, Task.class));
 		return false;
 	}
-	
-	//If user selects delete task remove it from UI and DB and if the task is a shared task
-	//remove it from the person who has the task too
-	public boolean removeTask(String taskId)
-	{
+
+	// If user selects delete task remove it from UI and DB and if the task is a
+	// shared task
+	// remove it from the person who has the task too
+	public boolean removeTask(String taskId) {
+		List<String> tasks = util.getTaskAcceptedFriends(taskId);
+		if (tasks.size() > 0) {
+			// for each user name send the task id to be deleted.
+		}
+		util.deleteItem(SimpleDbUtil.getCurrentUser(), taskId);
 		
+		drawUI();
 		return true;
 	}
 }
